2c9aa890fa8bdd45709b690cd44d657522fb6616
Lucky Fernanda Rachman <lucky.rachman@merakjaya.local>
2019-10-04 09:19:11 +0700

    salah port

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
 
-let port = 8000;
+let port = 8080;
 
 app.listen(port, () => {
     console.log('Server is up and running on port number ' + port);
